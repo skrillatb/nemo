116a5fa1ec2fa227b4eaf1d9ca6076978c495c77
dereje <getutadessedereje@icloud.com>
2025-04-15 17:24:58 +0200

    Add Chi router and middleware to main.go; update go.mod and go.sum for dependency

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-    fmt.Println("Hello, Nemo!")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+    r.Use(middleware.RequestID)
+    r.Use(middleware.RealIP)
+    r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+   
+		w.Write([]byte("welcome"))
+	})
+
+
+	http.ListenAndServe(":3000", r)
 }
