f5d816c08d9cb67f172d72d00ee905da1fe3e20f
dereje <getutadessedereje@icloud.com>
2025-04-15 18:17:06 +0200

    Implement database connection and add endpoint for listing recent sites in main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,24 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/skrillatb/nemo/internal/db"
+	"github.com/skrillatb/nemo/internal/handlers"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	database, err := db.Connect()
+	if err != nil {
+		fmt.Println("Erreur DB:", err)
+		return
+	}
+	defer database.Close()
+
+	app := &handlers.App{DB: database}
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-    r.Use(middleware.RequestID)
-    r.Use(middleware.RealIP)
-    r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Recoverer)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-   
 		w.Write([]byte("welcome"))
 	})
 
+	r.Get("/sites", app.ListRecentSites)
 
 	http.ListenAndServe(":3000", r)
 }
